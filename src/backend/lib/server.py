465db784aad33a3de858a52ccb54c9bca14aaef5
Mehmet Erçin DOĞAN <erindogan2001@hotmail.com>
2024-06-20 01:27:31 +0300

    server async and sync tests

--- src/backend/lib/server.py
+++ src/backend/lib/server.py
@@ -3,7 +3,7 @@ import asyncio
 import threading
 
 class Server:
-    def __init__(self,port) -> None:
+    def __init__(self,port=9999) -> None:
         self._port = port
         self._WSclients = []
         self.__connectedUsers = []
@@ -14,17 +14,16 @@ class Server:
         asyncio.get_event_loop().run_until_complete(serve)
         asyncio.get_event_loop().run_forever()
 
-
-    def _x(self):
-        print(self._WSclients)
-
     async def _clientHandler(self,ws):
         print(f"Websocket client connected from {ws.remote_address}")
         self._WSclients.append(ws.remote_address)
 
         try:
-            threading.Thread(target = self._x).run()
             while True:
-                pass
+                msg = await ws.recv()
         except:
             pass
+
+if __name__ == "__main__":
+    s1 = Server()
+    s1.run()
--- src/backend/main.py
+++ src/backend/main.py
@@ -2,13 +2,17 @@ from lib.server import Server
 import sys
 
 if __name__ == "__main__":
+    sys.argv.pop(0) # for removing source code file path
     # just port number can enter when application starting
     if len(sys.argv) <= 1:
-        port = 5000 # default port number
-        if len(sys.argv) == 1: # 
+        port = 5000  # default port number
+        if len(sys.argv) == 1:  # if has one argument
             try:
-                port = int(sys.argv[0])
+                port = int(sys.argv[0].strip())
+                # try to convert if there are not any problem at given value
+                # assign value to current port value which for going to use run
             except:
+                # if there are error program continues with default value
                 pass
         s1 = Server(port)
         # if do not given port address it selected defaultly as 5000
