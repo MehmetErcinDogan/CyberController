de90b17174b70460fb6e30a86b5833eda782afe5
Mehmet Erçin DOĞAN <erindogan2001@hotmail.com>
2024-06-21 01:25:06 +0300

    mid upload at 01.25am

--- src/backend/lib/server.py
+++ src/backend/lib/server.py
@@ -1,13 +1,19 @@
 import websockets
 import asyncio
-
+from database import Database
+import sys
 
 class Server:
     def __init__(self, port=9999) -> None:
-        self._port = port
-        self._WSclients = []
-        self.__connectedUsers = []
-
+        try:
+            self._port = port
+            self._WSclients = []
+            self._connectedID = []
+            self._db = Database()
+        except Exception as e:
+            print("Error occured during initialize server object as",e)
+            sys.exit()
+            
     def _run(self):
         print(f"Server started to listening on \"localhost:{self._port}\"")
         serve = websockets.serve(self._clientHandler, "localhost", self._port)
@@ -19,16 +25,11 @@ class Server:
         self._WSclients.append(ws.remote_address)
 
         try:
-            while True:
+            run = True
+            while run:
                 msg = await ws.recv()
-                if msg.lower() == "#EXIT": # msg handlerin geriye döünüşü ile bağlantı kurmaya bak
-                    print("WS connection closed",ws.remote_address)
-                    self._WSclients.remove(ws)
-                    await ws.close()
-                    
-                
-                await self._mssgHandler(msg)
-
+                tag, msg = await Server._parser()
+                run = await self._mssgHandler(tag, msg, ws)
         except websockets.ConnectionClosed:
             self._WSclients.remove(ws)
             print(f"WS Client from {ws.remote_address} disconnected")
@@ -40,8 +41,46 @@ class Server:
                 await ws.close()
                 print(f"WS Connection closed {ws.remote_address}")
 
-    async def _mssgHandler(self,msg):
-        return
+    async def _mssgHandler(self, tag, msg, ws): # NOT FİNİSHED
+        try:
+            if msg == "#CHECK":
+                if len(tag) != 1:
+                    raise Exception("Wrong tag value")
+                else:
+                    try:
+                        self._connectedID.index(tag[0])
+                        await ws.send("#ALLOW")
+                        return True
+                    except ValueError:
+                        await ws.send("#DENY")
+                        return False
+            elif msg == "#SIGN":
+                username = tag[0]
+                password = tag[1]
+                if self._db.checkUser(username,password):
+                    await ws.send("#ALLOW")
+                    return True
+                else:
+                    await ws.send("#DENY")
+                    return False
+                
+        except Exception as e:
+            print("Error occured at msgHandler as", e)
+            return False
+        
+    async def _parser(msg):
+        try:
+            msg = msg.strip()
+            if msg[0] != "#":
+                raise Exception("Comming Message Error")
+            else:
+                _ = msg.find(' ')
+                ms = msg[:_].strip().upper()
+                tag = msg[_+1, :].strip().split()
+                return (tag, ms)
+        except Exception as e:
+            print("Error occured at parser as", e)
+
 
 if __name__ == "__main__":
     s1 = Server()
